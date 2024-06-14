ad0276f389e3b41ec03f1675375b6d84f526e715
ミツ・トモヤス <mitsutomoyasu@mitsutomoyasunoMacBook-Air.local>
2024-06-14 13:53:12 +0900

    投稿画面の内容を修正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,4 +6,13 @@ class ApplicationRecord < ActiveRecord::Base
     return nil if self.send(enum_name).nil?
     I18n.t!("enums.#{self.model_name.i18n_key}.#{enum_name}.#{self.send(enum_name)}")
   end
+
+  # enumの値（全てのkeyのハッシュ）をi18nの形の配列で返すメソッド
+  # フォームのセレクトボックスで使えるように、[表示名（i18n）, key]の形にしている
+  # send(enum_name.to_s.pluralize)で、enumの値を全て取得している
+  def self.enum_i18n(enum_name)
+    self.send(enum_name.to_s.pluralize).keys.map do |key|
+      [I18n.t("enums.#{self.model_name.i18n_key}.#{enum_name}.#{key}"), key]
+    end
+  end
 end
